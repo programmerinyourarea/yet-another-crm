be8e79e4c53e3aa84d4c49582d8d4d05c06f007e
programmerinyourarea <170766994+programmerinyourarea@users.noreply.github.com>
2024-11-29 02:39:50 +0100

    welcome page fix

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
